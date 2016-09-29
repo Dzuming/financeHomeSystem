e2ceed2231b5524713a0dd22e6236799348015ac
Dawid <Dawid>
2016-09-29 18:46:18 +0200

    Add new database

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
 var express = require('express');
-var MongoClient = require('mongodb').MongoClient;
-var app = express();
 
+var mongoose = require('mongoose');
+var app = express();
+app.use(express.static(__dirname + '/server'));
 app.use(express.static(__dirname + '/client'));
-var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
-MongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
-  if (err) {
-    throw err;
-  }
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/finance-system')
+var productSchema = mongoose.Schema({
+  name: String,
+  category: String,
+  Spending: Number,
 });
+var Product = mongoose.model('Product', productSchema);
+// var silence = new Product({
+//   name: 'Silence',
+//   category: 'jedzenie',
+//   Spending: 30
+
+// });
+// silence.save(function (err, silence) {
+//   if (err) return console.error(err);
+//   "error";
+// });
+app.get('/finance-system/Product', function(req, res) {
+
+        // use mongoose to get all todos in the database
+        Product.find(function(err, Product) {
+
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err)
+
+            res.json(Product); // return all todos in JSON format
+        });
+    });
+var server = app.listen(3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+})
