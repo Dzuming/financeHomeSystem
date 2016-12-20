ed7f0db3d63bd53a159eb207ec0c154a337ef8b7
Dawid <Dawid>
2016-12-20 22:38:26 +0100

    Create sample d3 v4 object

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ProductService } from './product/product.service';
 import { ProductPipe } from './product/product.pipe';
+import { ChartComponent } from './chart/chart.component';
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ProductPipe
+    ProductPipe,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/chart/chart.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, OnChanges, ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
+import * as d3 from 'd3';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ChartComponent implements OnInit, OnChanges {
+  @ViewChild('chart') private chartContainer: ElementRef;
+  @Input() private data: Array<any>;
+  private margin: any = { top: 20, bottom: 20, left: 20, right: 20};
+  private chart: any;
+  private width: number;
+  private height: number;
+  private xScale: any;
+  private yScale: any;
+  private colors: any;
+  private xAxis: any;
+  private yAxis: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.createChart();
+    if (this.data) {
+      // this.updateChart();
+    }
+  }
+
+  ngOnChanges() {
+    if (this.chart) {
+      // this.updateChart();
+    }
+  }
+
+  createChart() {
+  var jsonCircles = [
+  { "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
+   { "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
+   { "x_axis": 110, "y_axis": 100, "radius": 20, "color" : "red"}];
+ 
+ var svgContainer = d3.select("body").append("svg")
+                                     .attr("width", 200)
+                                     .attr("height", 200);
+
+var circles = svgContainer.selectAll("circle")
+                          .data(jsonCircles)
+                          .enter()
+                          .append("circle");
+
+var circleAttributes = circles
+                       .attr("cx", function (d) { return d.x_axis; })
+                       .attr("cy", function (d) { return d.y_axis; })
+                       .attr("r", function (d) { return d.radius; })
+                       .style("fill", function(d) { return d.color; });
+  }
+}
