6171fe023da549a99b7a55bf335d27d921fa0518
Dawid <Dawid>
2017-01-16 23:46:01 +0100

    Change table style

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,7 +1,7 @@
 //TODO: Update chart
 //TODO: Update onChange - change detection
 //TODO Function to getData
-import { Component, OnInit, OnChanges, ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
 import { RestService } from '../shared/services/rest.service';
 import { ChartService } from '../shared/services/chart.service';
@@ -11,22 +11,13 @@ import { ChartService } from '../shared/services/chart.service';
   styleUrls: ['./chart.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ChartComponent implements OnInit, OnChanges {
+export class ChartComponent implements OnInit {
   @ViewChild('chart') private chartContainer: ElementRef;
   private data: Array<any>;
   private errorMessage: Array<any>;
   public constructor(private restService: RestService, private chartService: ChartService) { }
   ngOnInit() {
     this.getData();
-
-    // setInterval(() => {
-    //   this.getData();
-    // }, 5000)
-
-  }
-
-  ngOnChanges() {
-
   }
   private getData() {
     this.restService.getProducts().subscribe(
