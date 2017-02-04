fb400034c5129bc653f84566d3934498bcc20bc9
Dawid <Dawid>
2017-02-04 11:44:23 +0100

    Add server side

---
+++ finance-system-client/src/app/login/login.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AuthenticationService } from '../shared/services/authentication.service';
+import { Router } from '@angular/router';
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+})
+export class LoginComponent implements OnInit {
+    private errorMessage;
+    private formErrors = {
+        'Name': '',
+        'Password': '',
+    };
+    public loginForm: FormGroup;
+    constructor(private formBuilder: FormBuilder, public authenticationService: AuthenticationService, private router: Router) {
+        this.authenticationService.logout();
+    }
+
+    ngOnInit() {
+        this.buildForm();
+    }
+    public buildForm(): void {
+        this.loginForm = this.formBuilder.group({
+            Name: [this.formErrors.Name, [
+                Validators.required,
+                Validators.minLength(4),
+                Validators.maxLength(24)
+            ]],
+            Password: [this.formErrors.Password, [
+                Validators.required,
+            ]]
+        });
+        // this.loginForm.valueChanges
+        //     .subscribe(data => this.onValueChanged(data));
+
+        // this.onValueChanged();
+
+    }
+    public login() {
+
+        if (!this.loginForm.value) {
+            this.buildForm();
+            return;
+        }
+        this.authenticationService.login(this.loginForm.value)
+            .subscribe(
+            data => {
+                if (data) {
+                    this.router.navigateByUrl('product');
+                }
+            },
+            error => this.errorMessage = <any>error);
+    }
+}
---
+++ finance-system-client/src/app/product/product-chart.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
+import * as d3 from 'd3';
+import { RestService } from '../shared/services/rest.service';
+import { ChartService } from '../shared/services/chart.service';
+import { CalculateService } from '../shared/services/calculate.service';
+@Component({
+  moduleId: 'module.id',
+  selector: 'app-chart',
+  templateUrl: './product-chart.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+export class ChartComponent implements OnInit {
+  @ViewChild('chart') private chartContainer: ElementRef;
+  private data: Array<any>;
+  private errorMessage: Array<any>;
+  public constructor(private restService: RestService, private chartService: ChartService, private calculateService: CalculateService) { }
+  ngOnInit() {
+    this.getData(this.calculateService.filterDate);
+  }
+  private getData(filter) {
+    this.restService.getProducts(filter).subscribe(
+      data => this.data = data,
+      error => this.errorMessage = <any>error,
+      () => {
+        this.chartService.createChart(this.data);
+      });
+  }
+}
