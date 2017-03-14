96400ad6988d90987dd7bb3d4d24bc766818f2ed
POLISZAD <dawid.poliszak@contractors.roche.com>
2017-03-14 13:23:00 +0100

    Add data to modal

--- finance-system-client/src/app/shared/side-nav/side-nav-date-modal/side-nav-date-modal.component.ts
+++ finance-system-client/src/app/shared/side-nav/side-nav-date-modal/side-nav-date-modal.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CalculateService } from '../../services/calculate.service';
+import { ChartService } from '../../services/chart.service';
+import { RestService } from '../../services/rest.service';
+import { Product } from '../../models/product.model';
 @Component({
   selector: 'app-side-nav-date-modal',
   templateUrl: './side-nav-date-modal.component.html',
-  styleUrls: ['./side-nav-date-modal.component.css']
+  styleUrls: ['./side-nav-date-modal.component.scss']
 })
 export class SideNavDateModalComponent implements OnInit {
-
-  constructor() { }
+  public product: Product[];
+  private errorMessage: string;
+  constructor(private calculateService: CalculateService, private chartService: ChartService,
+    private restService: RestService, ) { }
 
   ngOnInit() {
   }
-
+  public getProducts(filter) {
+    this.restService.getProducts(filter)
+      .subscribe(
+      (data: Product[]) => {
+        this.product = data;
+      },
+      error => this.errorMessage = <any>error,
+      () => {
+        this.restService.setProduct(this.product);
+        this.chartService.updateChart(this.product);
+      });
+  }
+  public setDate(event) {
+    let target = event.target || event.srcElement || event.currentTarget;
+    let dataAttr = target.attributes.data;
+    let value = dataAttr.nodeValue;
+    this.calculateService.filterDate = value;
+    this.getProducts(value);
+    console.log(value)
+  }
 }
--- finance-system-client/src/app/shared/side-nav/side-nav.component.ts
+++ finance-system-client/src/app/shared/side-nav/side-nav.component.ts
@@ -1,9 +1,6 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { CalculateService } from '../services/calculate.service';
-import { ChartService } from '../services/chart.service';
-import { RestService } from '../services/rest.service';
-import { Product } from '../models/product.model';
 import { ModalDirective } from 'ng2-bootstrap/modal';
 @Component({
   selector: 'app-side-nav',
@@ -13,31 +10,16 @@ import { ModalDirective } from 'ng2-bootstrap/modal';
 export class SideNavComponent implements OnInit {
   @ViewChild('childModal') public childModal: ModalDirective;
   public navigateUrl: Object = {};
-  private errorMessage: string;
   private filterDate: string;
   private getUrlPath: any;
-  public product: Product[];
+  
   constructor(
-    private chartService: ChartService,
-    private restService: RestService,
     public calculateService: CalculateService,
     private router: Router) { }
   ngOnInit() {
     this.getUrlPath = this.router.events.subscribe(
       () => this.changeNavigateUrl(this.router.url)
     );
-  }
-  public getProducts(filter) {
-    this.restService.getProducts(filter)
-      .subscribe(
-      (data: Product[]) => {
-        this.product = data;
-      },
-      error => this.errorMessage = <any>error,
-      () => {
-        this.restService.setProduct(this.product);
-        this.chartService.updateChart(this.product);
-      });
   }
   public changeNavigateUrl(url?: string) {
     const options = [{
