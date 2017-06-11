879f31d1315942f60c30a1c4068efe8718ffcef4
Dzuming <dawidpoliszak@op.pl>
2017-06-11 08:20:41 +0200

    Add data to dumb component

--- finance-system-client/src/app/login/login-form/login-form.component.ts
+++ finance-system-client/src/app/login/login-form/login-form.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AuthenticationService } from '../../shared/services/authentication.service';
+import { BehaviorService } from '../../shared/services/behavior.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  loginForm: FormGroup;
+  @Output() onLogin = new EventEmitter();
+  private formErrors = {
+    'Email': '',
+    'Password': ''
+  };
+  constructor(
+    private formBuilder: FormBuilder,
+    private behaviorService: BehaviorService,
+    private authenticationService: AuthenticationService,
+    private router: Router) {
   }
 
+  ngOnInit() {
+    this.buildForm();
+  }
+  buildForm(): void {
+    this.loginForm = this.formBuilder.group({
+      Email: [this.formErrors.Email, [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(24)
+      ]],
+      Password: [this.formErrors.Password, [
+        Validators.required,
+      ]]
+    });
+  }
+  login(): void {
+    if (!this.loginForm.value) {
+      this.buildForm();
+      return;
+    } else {
+      this.onLogin.emit(this.loginForm.value)
+    }
+
+  }
 }
--- finance-system-client/src/app/login/login.component.ts
+++ finance-system-client/src/app/login/login.component.ts
@@ -9,47 +9,24 @@ import { Router } from '@angular/router';
     styleUrls: ['login.component.scss']
 })
 export class LoginComponent implements OnInit {
-    loginForm: FormGroup;
     private errorMessage: string;
-    private formErrors = {
-        'Email': '',
-        'Password': ''
-    };
     constructor(
-        private formBuilder: FormBuilder,
         private behaviorService: BehaviorService,
         private authenticationService: AuthenticationService,
         private router: Router) {
     }
 
     ngOnInit() {
-        this.buildForm();
     }
-    buildForm(): void {
-        this.loginForm = this.formBuilder.group({
-            Email: [this.formErrors.Email, [
-                Validators.required,
-                Validators.minLength(4),
-                Validators.maxLength(24)
-            ]],
-            Password: [this.formErrors.Password, [
-                Validators.required,
-            ]]
-        });
-    }
-    login(): void {
-        if (!this.loginForm.value) {
-            this.buildForm();
-            return;
+    authenticateUser(loginFormValues) {
+        this.authenticationService.login(loginFormValues)
+      .subscribe(
+      data => {
+        if (data) {
+          this.behaviorService.setUser(true);
+          this.router.navigateByUrl('product/Spending');
         }
-        this.authenticationService.login(this.loginForm.value)
-            .subscribe(
-            data => {
-                if (data) {
-                    this.behaviorService.setUser(true);
-                    this.router.navigateByUrl('product/Spending');
-                }
-            },
-            error => this.errorMessage = <any>error);
+      },
+      error => this.errorMessage = <any>error);
     }
 }
