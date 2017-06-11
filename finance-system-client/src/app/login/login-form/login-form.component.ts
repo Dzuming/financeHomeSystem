58efab69e084abf62a3bb01a158223892a1f58e9
Dzuming <dawidpoliszak@op.pl>
2017-06-11 11:38:26 +0200

    Add error to login component

--- finance-system-client/src/app/login/login-form/login-form.component.ts
+++ finance-system-client/src/app/login/login-form/login-form.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
 import { AuthenticationService } from '../../shared/services/authentication.service';
 import { BehaviorService } from '../../shared/services/behavior.service';
-import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
@@ -11,15 +10,25 @@ import { Router } from '@angular/router';
 export class LoginFormComponent implements OnInit {
   loginForm: FormGroup;
   @Output() onLogin = new EventEmitter();
-  private formErrors = {
+  formFields = {
     'Email': '',
     'Password': ''
+  };
+  private errorMessage: string;
+  private validationMessages = {
+    'Email': {
+      'required': 'Description is required.',
+      'minlength': 'Description must be at least 4 characters long.',
+    },
+    'Password': {
+      'required': 'Password is required.',
+      'minlength': 'Password must be at least 4 characters long.',
+    }
   };
   constructor(
     private formBuilder: FormBuilder,
     private behaviorService: BehaviorService,
-    private authenticationService: AuthenticationService,
-    private router: Router) {
+    private authenticationService: AuthenticationService) {
   }
 
   ngOnInit() {
@@ -27,15 +36,19 @@ export class LoginFormComponent implements OnInit {
   }
   buildForm(): void {
     this.loginForm = this.formBuilder.group({
-      Email: [this.formErrors.Email, [
+      Email: [this.formFields.Email, [
         Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(24)
+        Validators.minLength(4)
       ]],
-      Password: [this.formErrors.Password, [
+      Password: [this.formFields.Password, [
         Validators.required,
+        Validators.minLength(4)
       ]]
     });
+    this.loginForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
   }
   login(): void {
     if (!this.loginForm.value) {
@@ -43,7 +56,35 @@ export class LoginFormComponent implements OnInit {
       return;
     } else {
       this.onLogin.emit(this.loginForm.value)
+      if (this.loginForm.value) {
+            this.loginForm.reset();
+        }
     }
+  }
+  onValueChanged(data?: string): void {
+    if (!this.loginForm) { return; }
+    const form = this.loginForm;
+    for (const field in this.formFields) {
+      if (this.formFields.hasOwnProperty(field)) {
+        this.formFields[field] = '';
+        this.checkErrorValidate(form, field);
+      }
+    }
+  }
 
+  checkErrorValidate(form: FormGroup, field: string): void {
+    const control = form.get(field);
+    if (control && control.dirty && !control.valid) {
+      this.addError(control, field);
+    }
+  }
+
+  addError(control: AbstractControl, field: string): void {
+    const messages = this.validationMessages[field];
+    for (const key in control.errors) {
+      if (control.errors.hasOwnProperty(key)) {
+        this.formFields[field] += messages[key] + ' ';
+      }
+    }
   }
 }
--- finance-system-client/src/app/login/login.module.ts
+++ finance-system-client/src/app/login/login.module.ts
@@ -1,10 +1,13 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginFormComponent } from './login-form/login-form.component';
+
 @NgModule({
   imports: [
+    CommonModule ,
     FormsModule,
     ReactiveFormsModule,
     LoginRoutingModule
